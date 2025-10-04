b25a8fb6946a84a80981e03895503a113e38fc00
j-dio <tcshs.lumacang.johndio.a@gmail.com>
2025-10-05 00:25:26 +0800

    Finished

--- script.js
+++ script.js
@@ -50,7 +50,17 @@ function showResults(userOption) {
     optionsContainer.style.display = "none";
   }
 }
-function resetGame() {}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+  winnerMsgElement.innerText = "";
+  roundResultsMsg.innerText = "";
+}
 
 resetGameBtn.addEventListener("click", resetGame);
 
